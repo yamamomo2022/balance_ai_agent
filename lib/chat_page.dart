64eb69ffc3eaaf394e57510c875545587ac68a2a
viole <violetraisondetre@gmai.com>
2025-02-15 21:11:22 +0900

    feat: 🎸 create_chat_input_widget

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'genkit_client.dart';
+import 'widgets/chat_input_widget.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -11,9 +12,7 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  // Controller to manage input in the text field
   final TextEditingController _controller = TextEditingController();
-
   final dio = Dio();
   late final GenkitClient _genkitClient;
 
@@ -23,9 +22,8 @@ class _ChatPageState extends State<ChatPage> {
     _genkitClient = GenkitClient(dio: dio);
   }
 
-  // Method to send the input text
-  void _sendText() async {
-    final inputText = _controller.text;
+  // 新たに送信処理を関数化
+  void _handleSend(String inputText) async {
     if (inputText.isNotEmpty) {
       try {
         final responseText =
@@ -60,19 +58,9 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              width: 300,
-              child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  hintText: 'Enter text to send',
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _sendText,
-              child: const Icon(Icons.send),
+            ChatInputWidget(
+              controller: _controller,
+              onSend: _handleSend,
             ),
           ],
         ),
