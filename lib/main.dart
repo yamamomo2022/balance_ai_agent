321cc1f0af752269778e2a3093a846c9916a29de
viole <violetraisondetre@gmai.com>
2025-02-18 22:36:49 +0900

    feat: 🎸 disable debug banner in main app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Balance AI Agent',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
