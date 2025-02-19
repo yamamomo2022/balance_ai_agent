18e868d389532cd0054855af6a3c50499aed8526
viole <violetraisondetre@gmai.com>
2025-02-19 21:18:21 +0900

    feat: 🎸 add custom app bar and navigation to lifestyle page in chat room

--- lib/chat_room_page.dart
+++ lib/chat_room_page.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 
+import 'lifestyle_page.dart';
+import 'widgets/custom_app_bar.dart';
+
 String randomString() {
   final random = Random.secure();
   final values = List<int>.generate(16, (i) => random.nextInt(255));
@@ -22,8 +25,33 @@ class ChatRoomPageState extends State<ChatRoomPage> {
   final List<types.Message> _messages = [];
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
 
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: CustomAppBar(
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.assignment_outlined),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LifestylePage()),
+                );
+              },
+            ),
+          ],
+        ),
         body: Chat(
           user: _user,
           messages: _messages,
@@ -35,16 +63,5 @@ class ChatRoomPageState extends State<ChatRoomPage> {
     setState(() {
       _messages.insert(0, message);
     });
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomString(),
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
   }
 }
