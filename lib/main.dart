7033b5c5ea5acdd721aef18b1c5d93644dd847d0
viole <violetraisondetre@gmai.com>
2025-02-23 14:08:20 +0900

    feat: update iOS platform version and integrate Firebase
    - Updated Podfile to set iOS platform version to 13.0
    - Modified main.dart to initialize Firebase on app startup
    - Updated Podfile.lock and pubspec.lock to include Firebase dependencies
    - Added firebase_auth dependency in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'pages/login_signup_page.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
