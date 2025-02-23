8a6cd2f5dc83d3cbf6afb4250f6e588254863fcb
viole <violetraisondetre@gmai.com>
2025-02-23 14:37:35 +0900

    feat: integrate Firebase configuration for Android and iOS
    - Added Google Services plugin to Android build.gradle and settings.gradle.
    - Created google-services.json for Android Firebase configuration.
    - Added GoogleService-Info.plist for iOS Firebase configuration.
    - Introduced firebase.json for FlutterFire CLI configuration.
    - Implemented firebase_options.dart for platform-specific Firebase options.
    - Fixed import typo in main.dart for firebase_options.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'pages/login_signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebsae_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaulyFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
