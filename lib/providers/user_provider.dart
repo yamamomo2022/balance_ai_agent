d58bbdf88436a35a137e963f6123fbc548767bb5
viole <violetraisondetre@gmai.com>
2025-02-27 22:00:31 +0900

    feat: implement user authentication with UserProvider and AuthWrapper
    - Added UserProvider to manage user authentication state.
    - Created AuthWrapper to handle authentication state changes and navigate to the appropriate page (ChatRoomPage or LoginSignupPage).
    - Updated main.dart to use MultiProvider for dependency injection.
    - Introduced user_provider.dart to encapsulate user-related logic and state management.

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
-class Userprovider with ChangeNotifier {
+class UserProvider with ChangeNotifier {
   User? _user;
   User? get user => _user;
   bool get isLoggedIn => _user != null;
