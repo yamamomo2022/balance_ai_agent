c6bbf1099f45e5530dd5dfee04c8e751e0083ac5
yamamomo <violetraisondetre@gmail.com>
2025-07-15 08:22:49 +0900

    chore:firebaseAdmin.ts の lint 対応を行った。

--- functions/firebaseAdmin.ts
+++ functions/firebaseAdmin.ts
@@ -1,17 +1,22 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import serviceAccountJson from "./service-account-key.json";
-import { CallableRequest } from 'firebase-functions/v2/https'
+import {CallableRequest} from "firebase-functions/v2/https";
 
 // Firebaseの初期化（すでに初期化済みなら再初期化しない）
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountJson as admin.ServiceAccount),
+    credential: admin.credential.cert(
+      serviceAccountJson as admin.ServiceAccount
+    ),
   });
 }
 
 export const verifyAuth = (request: CallableRequest): void => {
   if (!request.auth) {
-    throw new functions.https.HttpsError('unauthenticated', 'User is not authenticated');
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "User is not authenticated"
+    );
   }
 };
