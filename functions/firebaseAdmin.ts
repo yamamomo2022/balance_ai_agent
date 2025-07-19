6654f9394c671a044c1259289feada2270b8b988
yamamomo <violetraisondetre@gmail.com>
2025-07-19 19:57:02 +0900

    feat:service-account-key.jsonのimportをやめた

--- functions/firebaseAdmin.ts
+++ functions/firebaseAdmin.ts
@@ -1,6 +1,13 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import serviceAccountJson from "./service-account-key.json";
+import * as path from "path";
+import * as fs from "fs";
+
+// サービスアカウントキーのパスを指定して動的に読み込む
+const serviceAccountPath = path.join(__dirname, "service-account-key.json");
+const serviceAccountJson = JSON.parse(
+  fs.readFileSync(serviceAccountPath, "utf8")
+);
 import {CallableRequest} from "firebase-functions/v2/https";
 
 // Firebaseの初期化（すでに初期化済みなら再初期化しない）
