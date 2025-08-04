1fd779424ab6ad81f6f4c07deeebfe02219d5504
yamamomo <violetraisondetre@gmail.com>
2025-08-05 07:54:08 +0900

    feat:lifestylelistapgeの雛形を作成した。

---
+++ lib/views/lifestyle_list_page.dart
@@ -0,0 +1,22 @@
+import 'package:balance_ai_agent/views/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+/// LifestyleListPage is a simple page that displays a list of lifestyle items.
+class LifestyleListPage extends StatelessWidget {
+  /// Constructor for LifestyleListPage
+  const LifestyleListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Center(
+        child: ListView.builder(itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item $index'),
+          );
+        }),
+      ),
+    );
+  }
+}
